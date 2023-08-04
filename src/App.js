dbce01ea603b1fbd70b8907bfb3441d5f31c2bb9
sydney-rd <sydneydavid56@gmail.com>
2023-08-04 13:43:13 -0400

    re-doing this project/adding NASA API fetch/trivia ques

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import './App.css';
-import { Routes, Route } from "react-router-dom"
-import Trivia from "./pages/Trivia/Trivia.jsx"
-import Home from "./pages/Homepage/Home.jsx"
-import Create from "./pages/Create/Create.jsx"
-import Navbar from "./components/Nav.jsx" // import the Navbar component
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Trivia from "./pages/Trivia/Trivia.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Create from "./pages/Create/Create.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> {/* render the Navbar component */}
       <Routes>
-          <Route path="/trivia" element={<Trivia />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/create" element={<Create />} />
+        <Route path="/trivia" element={<Trivia />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/create" element={<Create />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
-
-
-
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Home.css";
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+  const navigate = useNavigate();
+
+  const handleStartClick = () => {
+    navigate("/trivia");
+  };
+
+  return (
+    <div className="background-image-container">
+      <div className="rocket-container">
+        <img src="../../assets/dogBg.png" alt="rocket" className="rocket" />
+      </div>
+      <div className="saturn-container">
+        <img src="../../assets/Saturn.gif" alt="saturn" className="saturn" />
+      </div>
+      <div className="content-container">
+        <h1 className="main-title">Astronomy Trivia</h1>
+        <div className="main-summary">
+          Welcome to Astronomy Trivia. Utilizing the wonderous pictures from
+          NASA's Astronomy Picture of the Day, test your knowledge while
+          learning a thing or two!
+        </div>
+        <div className="start-btn" onClick={handleStartClick}>
+          Start
+        </div>
+      </div>
+    </div>
+  );
+}
+// https://raw.githubusercontent.com/sydney-rd/NASA-api-project/main/APOD.json
